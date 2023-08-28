5c6ad80e57a0759f349965e5af5423bb160afe5c
betül nar <betnarr@hotmail.com>
2023-08-28 17:24:54 +0300

    array

--- src/main/java/com/hamitmizrak/_05_Array.java
+++ src/main/java/com/hamitmizrak/_05_Array.java
@@ -2,23 +2,33 @@ package com.hamitmizrak;
 
 public class _05_Array {
     public static void main(String[] args) {
-        int [] dizi=new int[9];
-        dizi[1]=1;
-        dizi[4]=2;
-        for (int i=0 ;i<dizi.length ;i++ ){
+        int[] dizi = new int[9];
+        dizi[1] = 1;
+        dizi[4] = 2;
+
+        // Dizinin tüm elemanlarını ekrana yazdıralım
+        System.out.println("Dizi Elemanları:");
+        for (int i = 0; i < dizi.length; i++) {
             System.out.println(dizi[i]);
         }
-        System.out.println(dizi.length+"dizinin eleman sayısı");
 
+        // Dizinin eleman sayısını ekrana yazdıralım
+        System.out.println(dizi.length + " dizinin eleman sayısı");
 
+        // Dizideki elemanların toplamını hesaplayalım
+        int toplam = 0;
+        for (int i = 0; i < dizi.length; i++) {
+            toplam += dizi[i];
+        }
+        System.out.println("Dizideki Elemanların Toplamı: " + toplam);
 
-
-
+        // Dizideki en büyük elemanı bulalım
+        int max = dizi[0];
+        for (int i = 1; i < dizi.length; i++) {
+            if (dizi[i] > max) {
+                max = dizi[i];
+            }
+        }
+        System.out.println("Dizideki En Büyük Eleman: " + max);
     }
-
-
-
-
-
-
 }
