563cb849a73a9633b14dc900f56359e11e0bb9be
betül nar <betnarr@hotmail.com>
2023-08-28 18:12:39 +0300

    array düzenlemee

---
+++ src/main/java/com/hamitmizrak/Contry.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak;
+
+public class Contry {
+    public String getContryName() {
+        return contryName;
+    }
+
+    public void setContryName(String contryName) {
+        this.contryName = contryName;
+    }
+
+    private String contryName;
+
+
+
+}
--- src/main/java/com/hamitmizrak/_05_Array.java
+++ src/main/java/com/hamitmizrak/_05_Array.java
@@ -12,23 +12,6 @@ public class _05_Array {
             System.out.println(dizi[i]);
         }
 
-        // Dizinin eleman sayısını ekrana yazdıralım
-        System.out.println(dizi.length + " dizinin eleman sayısı");
 
-        // Dizideki elemanların toplamını hesaplayalım
-        int toplam = 0;
-        for (int i = 0; i < dizi.length; i++) {
-            toplam += dizi[i];
-        }
-        System.out.println("Dizideki Elemanların Toplamı: " + toplam);
-
-        // Dizideki en büyük elemanı  bulalım
-        int max = dizi[0];
-        for (int i = 1; i < dizi.length; i++) {
-            if (dizi[i] > max) {
-                max = dizi[i];
-            }
-        }
-        System.out.println("Dizideki En Büyük Eleman: " + max);
     }
 }
